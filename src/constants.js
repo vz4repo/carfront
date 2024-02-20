1760701f5519935b04805bc7f043314bc8c01fc2
vz-mac <thevzforalt@gmail.com>
2024-02-20 16:59:07 +0900

    FIX:: publicIP to privateIP

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 // export const SERVER_URL='http://localhost:8080/';
-export const SERVER_URL='http://52.78.204.44:8082/';
+// export const SERVER_URL='http://52.78.204.44:8082/';
+export const SERVER_URL='http://172.31.40.87:8082/';
